e4447e4b9abf43356c366188f24158a0d8577aba
Shah Yawar <yawer@example.com>
2026-01-02 20:54:46 +0530

    Configured app for deployment

--- app.py
+++ app.py
@@ -41,6 +41,11 @@ def predict():
             success=False
         )
 
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
+
